f083631b2e849426e4d4a5623b626117fe1c7e68
Kseniya Berlyavskaya <berlyavskaya-kv@yandex.ru>
2019-09-12 20:16:10 +0300

    дз4

--- slider-reviews.js
+++ slider-reviews.js
@@ -2,19 +2,10 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
-
 /* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    slides.className.add ("review__item--current");
 }
 
 /* Основная функция слайдера */
